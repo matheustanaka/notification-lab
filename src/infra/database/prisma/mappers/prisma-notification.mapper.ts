f513a6542cee0f6b8c22462ea8cdaa36ba4097af
Matheus Tanaka <matheustanakanog@gmail.com>
2022-12-19 23:23:56 -0300

    feat: create mapper to domain

--- src/infra/database/prisma/mappers/prisma-notification.mapper.ts
+++ src/infra/database/prisma/mappers/prisma-notification.mapper.ts
@@ -1,6 +1,12 @@
-import { Notification } from '@app/entities/notification';
+import { Notification as RawNotification } from '@prisma/client';
+import { Notification } from '../../../../app/entities/notification';
+import { Content } from '../../../../app/entities/content';
 
+// Converte o notification da camada de aplicacao para a camada de banco de dados.
+// O notification da camada de aplicacao é diferente da entidade que está no banco de dados.
+// Para camada de banco de dados reconhecer esse notification da aplicacao é necessário converte-lo através de mappers.
 export class PrismaNotificationMapper {
+  // Converte para o prisma
   static toPrisma(notification: Notification) {
     return {
       id: notification.id,
@@ -10,5 +16,19 @@ export class PrismaNotificationMapper {
       readAt: notification.readAt,
       createdAt: notification.createdAt,
     };
+  }
+  // Converte para a aplicacao
+  static toDomain(raw: RawNotification): Notification {
+    return new Notification(
+      {
+        category: raw.category,
+        content: new Content(raw.content),
+        recipientId: raw.recipientId,
+        readAt: raw.readAt,
+        canceledAt: raw.canceledAt,
+        createdAt: raw.createdAt,
+      },
+      raw.id,
+    );
   }
 }
