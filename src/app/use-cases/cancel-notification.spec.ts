f9faee56c2ff7dda083470d9c00ff2e3cc89b869
Matheus Tanaka <matheustanakanog@gmail.com>
2022-12-19 22:04:23 -0300

    feat: add factory pattern

--- src/app/use-cases/cancel-notification.spec.ts
+++ src/app/use-cases/cancel-notification.spec.ts
@@ -3,17 +3,14 @@ import { Notification } from '../entities/notification';
 import { InMemoryNotificationsRepository } from '../../../test/repositories/in-memory-notifications-repository';
 import { CancelNotification } from './cancel-notification';
 import { NotificationNotFound } from './errors/notification-not-found';
+import { makeNotification } from '@test/factories/notification-factory';
 
 describe('Cancel notification', () => {
   it('should be able to cancel a notification', async () => {
     const notificationsRepository = new InMemoryNotificationsRepository();
     const cancelNotification = new CancelNotification(notificationsRepository);
 
-    const notification = new Notification({
-      category: 'social',
-      content: new Content('Nova solicitacao de amizade!'),
-      recipientId: '1-example',
-    });
+    const notification = makeNotification();
 
     await notificationsRepository.create(notification);
 
