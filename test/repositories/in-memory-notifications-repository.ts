1412c97d5125ed91fe5c073c37b9587074e06749
Matheus Tanaka <matheustanakanog@gmail.com>
2022-12-19 22:03:08 -0300

    feat: add method to  find and count by recipient id

--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -10,6 +10,7 @@ export class InMemoryNotificationsRepository
   async create(notification: Notification) {
     this.notifications.push(notification);
   }
+
   async findById(notificationId: string): Promise<Notification | null> {
     const notification = this.notifications.find(
       (item) => item.id === notificationId,
@@ -19,6 +20,19 @@ export class InMemoryNotificationsRepository
 
     return notification;
   }
+
+  async findManyByRecipientId(recipientId: string): Promise<Notification[]> {
+    return this.notifications.filter(
+      (notification) => notification.recipientId === recipientId,
+    );
+  }
+
+  async countManyByRecipientId(recipientId: string): Promise<number> {
+    return this.notifications.filter(
+      (notification) => notification.recipientId === recipientId,
+    ).length;
+  }
+
   async save(notification: Notification): Promise<void> {
     const notificationIndex = this.notifications.findIndex(
       (item) => item.id === notification.id,
